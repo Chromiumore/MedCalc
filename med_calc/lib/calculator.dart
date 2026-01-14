05157a928c47f83dbe39ae8a772fa984826a861d
Chromiumore <parts.dima06@gmail.com>
2026-01-14 21:17:59 +0300

    calculator input layout

--- med_calc/lib/calculator.dart
+++ med_calc/lib/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculatorScreen extends StatelessWidget {
   const CalculatorScreen({super.key});
@@ -6,8 +7,63 @@ class CalculatorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
+      body: Padding(
+        padding: const EdgeInsets.all(60),
+        child: Column(
+          spacing: 15,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CheckboxListTile(
+              title: const Text('Диализ не менее двух раз за последние 7 дней'),
+              value: true,
+              onChanged: (bool? newValue) => ()
+            ),
+
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')), 
+              ],
+              decoration: InputDecoration(
+                labelText: 'Креатинин, мкмоль/л',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')), 
+              ],
+              decoration: InputDecoration(
+                labelText: 'Билирубин, мкмоль/л',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')), 
+              ],
+              decoration: InputDecoration(
+                labelText: 'МНО',
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            TextField(
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')), 
+              ],
+              decoration: InputDecoration(
+                labelText: 'Натрий, ммоль/л',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
