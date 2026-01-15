f2c418a24e43f3cf79c8f2bbf54dd0061127fb53
Chromiumore <parts.dima06@gmail.com>
2026-01-16 01:17:02 +0300

    MELD calculation class

---
+++ med_calc/lib/calculator.dart
@@ -0,0 +1,30 @@
+import 'dart:math';
+
+class Calculator {
+  static final double _creatineConversionFactor = 88.4;
+  static final double _bilirubinConversionFactor = 17.104;
+
+  static double _calculateOriginalMELD(
+    double creatinine,
+    double bilirubin,
+    double inr,
+  ) {
+    return (0.957 * log(creatinine / _creatineConversionFactor) + 0.378 * log(bilirubin / _bilirubinConversionFactor) + 1.12 * log(inr) + 0.643) * 10;
+  }
+
+  static int calculateMELD({
+    required double creatinine,
+    required double bilirubin,
+    required double inr,
+    required double natrium,
+    bool dialysisLastWeek = false
+  }) {
+    double originalMELD = _calculateOriginalMELD(creatinine, (dialysisLastWeek) ? 353.6 : bilirubin, inr);
+    double meld = originalMELD + 1.32 * (137 - natrium) - 0.033 * originalMELD * (137 - natrium);
+    return meld.round();
+  }
+
+  static double mortalityFromScore(int score) {
+    return 19.6;
+  }
+}
